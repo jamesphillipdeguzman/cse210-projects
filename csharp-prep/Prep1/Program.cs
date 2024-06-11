c819104d10b13cccdb8e608ad0df53f82a80b790
jamesphillipdeguzman <deg24001@byui.edu>
2024-06-11 21:06:57 +0800

    Modified Program.cs to display my name as De Guzman, James De Guzman

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name? ");
+        String fname = Console.ReadLine();
+        Console.WriteLine("What is your last name? ");
+        String lname = Console.ReadLine();
+        Console.Write($"Your name is {lname}, {fname} {lname}.");
     }
 }
