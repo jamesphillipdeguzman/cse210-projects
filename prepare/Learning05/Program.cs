b9532703029749e313275c87d980ac37bb79c4d2
jamesphillipdeguzman <deg24001@byui.edu>
2024-07-10 20:08:46 +0800

    Added Learning 05: Shapes (Polymorphism)

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using Learning05;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Calculate area of a square (e.g. side * side)
+        Square square = new Square("brown", 5.5);
+        string squareColor = square.GetColor();
+        double squareArea = square.GetArea();
+        Console.WriteLine(squareColor);
+        Console.WriteLine(squareArea);
+
+        // Calculate area of a rectangle (e.g. length * width)
+        Rectangle rectangle = new Rectangle("gray", 3, 5.5);
+        string rectColor = rectangle.GetColor();
+        double rectArea = rectangle.GetArea();
+        Console.WriteLine(rectColor);
+        Console.WriteLine(rectArea);
+
+        // Calculate area of a circle (e.g. PI * radius^2)
+        Circle circle = new Circle("yellow", 6);
+        string circleColor = circle.GetColor();
+        double circleArea = circle.GetArea();
+        Console.WriteLine(circleColor);
+        Console.WriteLine(circleArea);
+
+        // Create a list of shapes
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetArea());
+        }
+
     }
+
 }
---
+++ prepare/Learning05/Square.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Learning05;
+
+public class Square : Shape
+{
+    private double _side;
+    public Square(string color, double side) : base(color)
+    {
+        _side = side;
+
+    }
+
+    public override double GetArea()
+    {
+        return _side * _side;
+    }
+}
